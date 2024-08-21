f9c12a057fbbeb0ec15f32437dae5e77bfcbb805
Thiago Luis <thiago.luis88@gmail.com>
2024-08-20 22:48:31 -0300

    chore: edit vpc and eks

--- infra/eks.tf
+++ infra/eks.tf
@@ -1,49 +1,129 @@
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "20.23.0"
+# resource "aws_vpc" "eks_vpc" {
+#   cidr_block = "10.0.0.0/16"
 
-  cluster_name                   = local.name
-  cluster_endpoint_public_access = true
-  cluster_endpoint_private_access = false
+#   tags = {
+#     Name = "eks_vpc"
+#   }
+# }
 
-  cluster_addons = {
-    coredns = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
-    }
+# resource "aws_subnet" "eks_subnet_a" {
+#   vpc_id            = aws_vpc.eks_vpc.id
+#   cidr_block        = "10.0.1.0/24"
+#   availability_zone = "us-east-1a"  # Change according to your region
+
+#   tags = {
+#     Name = "eks_subnet_a"
+#   }
+# }
+
+# resource "aws_subnet" "eks_subnet_b" {
+#   vpc_id            = aws_vpc.eks_vpc.id
+#   cidr_block        = "10.0.2.0/24"
+#   availability_zone = "us-east-1b"  # Change according to your region
+
+#   tags = {
+#     Name = "eks_subnet_b"
+#   }
+# }
+
+resource "aws_eks_cluster" "fastfood-cluster" {
+  name     = "fastfood-cluster"
+  role_arn = "arn:aws:iam::714167738697:role/LabRole"
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.private-subnet-a.id,
+      aws_subnet.private-subnet-b.id,
+      aws_subnet.public-subnet-a.id,
+      aws_subnet.public-subnet-b.id
+    ]
   }
 
-  vpc_id                   = module.vpc.vpc_id
-  subnet_ids               = module.vpc.private_subnets
-  control_plane_subnet_ids = module.vpc.intra_subnets
-
-  # EKS Managed Node Group(s)
-  eks_managed_node_group_defaults = {
-    ami_type       = "ami-0ae8f15ae66fe8cda"
-    instance_types = ["t3.medium"]
-
-    attach_cluster_primary_security_group = true
-  }
-
-  eks_managed_node_groups = {
-    fastfood-node-group = {
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
-
-      instance_types = ["t3.medium"]
-      capacity_type  = "ON_DEMAND"
-
-      tags = {
-        ExtraTag = "fastfood-instance"
-      }
-    }
-  }
-
-  tags = local.tags
+  bootstrap_self_managed_addons = true
 }
+
+resource "aws_eks_node_group" "fastood_node_group" {
+  cluster_name    = aws_eks_cluster.fastfood-cluster.name
+  node_group_name = "fastood_node_group"
+  node_role_arn   = "arn:aws:iam::714167738697:role/LabRole"
+  subnet_ids      = [
+    aws_subnet.private-subnet-a.id,
+    aws_subnet.private-subnet-b.id
+  ]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
+  }
+}
+
+output "cluster_name" {
+  value = aws_eks_cluster.fastfood-cluster.name
+}
+
+# output "cluster_endpoint" {
+#   value = aws_eks_cluster.fastfood-cluster.endpoint
+# }
+
+# module "eks" {
+#   source  = "terraform-aws-modules/eks/aws"
+#   version = "20.23.0"
+
+#   cluster_name                   = local.name
+#   cluster_endpoint_public_access = true
+#   cluster_endpoint_private_access = false
+
+#   cluster_addons = {
+#     coredns = {
+#       most_recent = true
+#     }
+#     kube-proxy = {
+#       most_recent = true
+#     }
+#     vpc-cni = {
+#       most_recent = true
+#     }
+#   }
+
+#   vpc_id                   = module.vpc.vpc_id
+#   subnet_ids               = module.vpc.private_subnets
+#   control_plane_subnet_ids = module.vpc.intra_subnets
+
+#   # EKS Managed Node Group(s)
+#   eks_managed_node_group_defaults = {
+#     ami_type       = "ami-0ae8f15ae66fe8cda"
+#     instance_types = ["t3.medium"]
+
+#     attach_cluster_primary_security_group = true
+#   }
+
+#   eks_managed_node_groups = {
+#     fastfood-node-group = {
+#       min_size     = 1
+#       max_size     = 2
+#       desired_size = 1
+
+#       instance_types = ["t3.medium"]
+#       capacity_type  = "ON_DEMAND"
+
+#       tags = {
+#         ExtraTag = "fastfood-instance"
+#       }
+#     }
+#   }
+
+#   tags = local.tags
+# }
+
+# module "aws_auth" {
+#   source = "terraform-aws-modules/eks/aws//modules/aws-auth"
+#   manage_aws_auth_configmap = true
+#   aws_auth_roles = [
+#     {
+#       rolearn  = data.aws_iam_role.karpenter_instance.arn
+#       username = "system:node:{{EC2PrivateDNSName}}"
+#       groups   = ["system:bootstrappers", "system:nodes"]
+#     },
+#   ]
+#   aws_auth_users = var.eks_additional_users
+# }
--- infra/vpc.tf
+++ infra/vpc.tf
@@ -1,22 +1,165 @@
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.13.0"
+/* VPC */
+resource "aws_vpc" "fiap-vpc" {
+  cidr_block = "10.123.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
-  name = local.name
-  cidr = local.vpc_cidr
+  tags = {
+    Name = "fiap-vpc"
+    "kubernetes.io/cluster/${aws_eks_cluster.fastfood-cluster.name}" = "shared"
+  }
+}
 
-  azs             = local.azs
-  private_subnets = local.private_subnets
-  public_subnets  = local.public_subnets
-  intra_subnets   = local.intra_subnets
+# Create Elastic IP
+resource "aws_eip" "vpc-elastic-ip" {
+  vpc = true
+}
 
-  enable_nat_gateway = true
+/* Internet gateway for the public subnet */
+resource "aws_internet_gateway" "fiap-vpc-igw" {
+  vpc_id = "${aws_vpc.fiap-vpc.id}"
+  tags = {
+    Name        = "fiap-vpc-igw"
+  }
+}
 
-  public_subnet_tags = {
+/* NAT Gateway */
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.vpc-elastic-ip.id
+  subnet_id     = aws_subnet.public-subnet-a.id
+
+  tags = {
+    Name = "NAT Gateway for Custom Kubernetes Cluster"
+  }
+}
+
+/* Private Subnet A */
+resource "aws_subnet" "private-subnet-a" {
+  vpc_id            = aws_vpc.fiap-vpc.id
+  cidr_block        = "10.123.1.0/24"
+  availability_zone = "us-east-1a"
+
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "private-subnet-a"
+    "kubernetes.io/cluster/${aws_eks_cluster.fastfood-cluster.name}" = "shared"
+    "kubernetes.io/role/internal-elb" = 1
+  }
+}
+
+/* Private Subnet B */
+resource "aws_subnet" "private-subnet-b" {
+  vpc_id            = aws_vpc.fiap-vpc.id
+  cidr_block        = "10.123.2.0/24"
+  availability_zone = "us-east-1b" 
+
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "private-subnet-b"
+    "kubernetes.io/cluster/${aws_eks_cluster.fastfood-cluster.name}" = "shared"
+    "kubernetes.io/role/internal-elb" = 1
+  }
+}
+
+/* Public Subnet A */
+resource "aws_subnet" "public-subnet-a" {
+  vpc_id            = aws_vpc.fiap-vpc.id
+  cidr_block        = "10.123.3.0/24"
+  availability_zone = "us-east-1a"
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-a"
+    "kubernetes.io/cluster/${aws_eks_cluster.fastfood-cluster.name}" = "shared"
     "kubernetes.io/role/elb" = 1
   }
+}
 
-  private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = 1
+/* Public Subnet B */
+resource "aws_subnet" "public-subnet-b" {
+  vpc_id            = aws_vpc.fiap-vpc.id
+  cidr_block        = "10.123.4.0/24"
+  availability_zone = "us-east-1b" 
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-b"
+    "kubernetes.io/cluster/${aws_eks_cluster.fastfood-cluster.name}" = "shared"
+    "kubernetes.io/role/elb" = 1
+  }
+}
+
+/* Routing table for private subnet */
+resource "aws_route_table" "private-rt" {
+  vpc_id = "${aws_vpc.fiap-vpc.id}"
+  tags = {
+    Name        = "vpc-private-route-table"
+  }
+}
+
+/* Routing table for public subnet */
+resource "aws_route_table" "public-rt" {
+  vpc_id = "${aws_vpc.fiap-vpc.id}"
+  tags = {
+    Name        = "vpc-public-route-table"
+  }
+}
+
+resource "aws_route" "public_internet_gateway" {
+  route_table_id         = "${aws_route_table.public-rt.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.fiap-vpc-igw.id}"
+}
+
+/* Route table associations */
+resource "aws_route_table_association" "public-1a" {
+  # count          = "${length(var.public_subnets_cidr)}"
+  subnet_id      = "${aws_subnet.public-subnet-a.id}"
+  route_table_id = "${aws_route_table.public-rt.id}"
+}
+
+resource "aws_route_table_association" "public-1b" {
+  # count          = "${length(var.public_subnets_cidr)}"
+  subnet_id      = "${aws_subnet.public-subnet-b.id}"
+  route_table_id = "${aws_route_table.public-rt.id}"
+}
+
+resource "aws_route_table_association" "private-1a" {
+  # count          = "${length(var.private_subnets_cidr)}"
+  subnet_id      = "${aws_subnet.private-subnet-a.id}"
+  route_table_id = "${aws_route_table.private-rt.id}"
+}
+
+resource "aws_route_table_association" "private-1b" {
+  # count          = "${length(var.private_subnets_cidr)}"
+  subnet_id      = "${aws_subnet.private-subnet-b.id}"
+  route_table_id = "${aws_route_table.private-rt.id}"
+}
+
+/*==== VPC's Default Security Group ======*/
+resource "aws_security_group" "default" {
+  name        = "vpc-default-sg"
+  description = "Default security group to allow inbound/outbound from the VPC"
+  vpc_id      = "${aws_vpc.fiap-vpc.id}"
+  depends_on  = [aws_vpc.fiap-vpc]
+  ingress {
+    from_port = "0"
+    to_port   = "0"
+    protocol  = "-1"
+    self      = true
+  }
+  
+  egress {
+    from_port = "0"
+    to_port   = "0"
+    protocol  = "-1"
+    self      = "true"
+  }
+  tags = {
+    Environment = "Default security group"
   }
 }
