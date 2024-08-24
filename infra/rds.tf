7e3a1fef3e9e909ad5ccdf460e301de8e9e9d01c
Thiago Luis <thiago.luis88@gmail.com>
2024-08-24 01:44:15 -0300

    feat: adding AWS EC2 Instance

--- infra/rds.tf
+++ infra/rds.tf
@@ -56,3 +56,46 @@ resource "aws_security_group" "rds-security-group" {
     Name        = "rds-security-group"
   }
 }
+
+###########################
+########### EC2 ###########
+###########################
+
+# Create an IAM instance profile for the EC2 instance
+resource "aws_iam_instance_profile" "instance-profile" {
+  name = "ec2-instance-profile"
+  role = "LabRole"
+}
+
+resource "aws_instance" "rds-instance" {
+  ami = "ami-03a4942b8fcc1f29d" # <https://cloud-images.ubuntu.com/locator/ec2/> 
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.private-subnet[0].id
+  associate_public_ip_address = true
+  key_name                    = "vockey" # name from FIAP
+  iam_instance_profile        = aws_iam_instance_profile.instance-profile.name
+
+  vpc_security_group_ids = [
+    aws_security_group.rds-security-group.id
+  ]
+  root_block_device {
+    delete_on_termination = true
+    # iops                  = 150 # only valid for volume_type io1
+    volume_size = 50
+    volume_type = "gp2"
+  }
+  tags = {
+    Name = "instance-profile"
+    OS   = "ubuntu"
+  }
+
+  depends_on = [aws_security_group.rds-security-group]
+
+#   user_data = base64encode(templatefile("user_data.sh", {
+#     DB_USER = aws_db_instance.mysql_8.username
+#     DB_PASSWORD_PARAM = data.aws_ssm_parameter.db_password.name
+#     DB_HOST = aws_db_instance.mysql_8.address
+#     DB_PORT = aws_security_group_rule.allow_mysql_in.from_port
+#     DB_NAME = aws_db_instance.mysql_8.db_name
+#   }))
+}
